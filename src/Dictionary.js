12a06d475d46a9cbf10579a119fb80c8b0715120
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-05-03 13:53:11 +0930

    added axios. dictionary api call added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("")
+
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
@@ -11,6 +16,10 @@ export default function Dictionary() {
     function search(event) {
         event.preventDefault();
         alert(`Searching for the definition of ${keyword}`);
+
+        // documentation: https://dictionaryapi.dev/
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     return (
