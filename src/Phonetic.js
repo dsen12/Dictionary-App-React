a083d825bbce06f847d4be332d60012a2aefd2bf
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-05-04 13:12:21 +0930

    added phonetics to results incl. audio

---
+++ src/Phonetic.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>
+            <br />
+            {props.phonetics.text};
+        </div>
+    )
+}
