a74a72f3032cef539278ad9243cfefacf52e176a
dsen12 <115708991+dsen12@users.noreply.github.com>
2023-10-29 22:54:03 +1030

    made app responsive

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ export default function Photos(props) {
                 <div className="row g-3">
                     {props.photos.map(function (photo, index){
                         return (
-                            <div className="col-4" key={index}>
+                            <div className="col-12 col-sm-6 col-md-4" key={index}>
                                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                                     <img src={photo.src.landscape} className="img-fluid" alt="/" />
                                 </a>
